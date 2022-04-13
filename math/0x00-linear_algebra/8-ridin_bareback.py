b69e0c58f2f9fb240f9f7fb14a83e3d73acbf7d9
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-04-13 01:03:23 +0000

    updated variables to be easy readable

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -4,17 +4,19 @@
 
 def mat_mul(mat1, mat2):
     """performs matrix multiplication"""
-    matrix = []
-    number_of_rows = len(mat1[0])
-    number_of_columns = len(mat2)
-    if (number_of_rows == number_of_columns):
-        for i in range(len(mat1)):
-            col = []
-            for j in range(len(mat2[0])):
+    rowsInMat1 = len(mat1)
+    columnsInMat1 = len(mat1[0])
+    rowsInMat2 = len(mat2)
+    columnsInMat2 = len(mat2[0])
+    if (columnsInMat1 == rowsInMat2):
+        matrixProduct = []
+        for i in range(rowsInMat1):
+            row = []
+            for j in range(columnsInMat2):
                 element = 0
-                for k in range(len(mat1[0])):
+                for k in range(columnsInMat1):
                     element += mat1[i][k] * mat2[k][j]
-                col.append(element)    
-            matrix.append(col)
-        return matrix
+                row.append(element)
+            matrixProduct.append(row)
+        return matrixProduct
     return None
