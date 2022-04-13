78c2f6f8f805c21e00ad7c1542bdfca1d4490184
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-04-13 01:01:13 +0000

    updated files o follow pycodestyle

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -6,6 +6,6 @@ def add_matrices2D(mat1, mat2):
     """adds two matrices element-wise"""
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    col = len(mat1[0])
-    row = len(mat1)
-    return [[(mat1[i][j] + mat2[i][j]) for j in range(row)] for i in range(col)]
+    c = len(mat1[0])
+    r = len(mat1)
+    return [[(mat1[i][j] + mat2[i][j]) for j in range(r)] for i in range(c)]
