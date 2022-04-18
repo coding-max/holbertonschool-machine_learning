afbf835f67f8610b57199067a3399f26d8eef0d9
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-04-18 16:21:29 +0000

    updated task 7 - fixed case when dsimensions don’t match

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -4,6 +4,10 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """concatenates two matrices along a specific axis"""
+    same_columns = len(mat1[0]) == len(mat2[0])
+    same_rows = len(mat1) == len(mat2)
+    if ((axis == 0 and same_rows) or (axis == 1 and same_columns)):
+        return None
     matrix = None
     if (axis == 0):
         mat1_cpy = [element.copy() for element in mat1]
