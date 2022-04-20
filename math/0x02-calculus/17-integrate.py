f56ac669db2ed1c070fb7119d27ead055caef0ec
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-04-20 22:40:49 +0000

    updated task 17 - pycodestyle

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -7,9 +7,8 @@ def poly_integral(poly, C=0):
     if (not isinstance(poly, list) or
        (not isinstance(C, (int, float))) or any
        (not isinstance(x, (int, float)) for x in poly)):
-            return None
+        return None
     integral = [C]
     if poly != [0]:
         integral = [float(C)] + [poly[i] / (i + 1) for i in range(len(poly))]
     return [float(x) if not x.is_integer() else int(x) for x in integral]
-
