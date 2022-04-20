28a0ad9263a1077a2abd7b9b9024a5da420a9575
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-04-20 21:33:50 +0000

    updated task 9 - pycodestyle

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -3,7 +3,7 @@
 
 
 def summation_i_squared(n):
-    """calculates \sum_{i=1}^{n} i^2"""
+    """calculates sum_{i=1}^{n} i^2"""
     if not isinstance(n, int):
         return None
     return sum(map(lambda i: i ** 2, range(1, n + 1)))
