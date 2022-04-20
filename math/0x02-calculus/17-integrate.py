2b78d64a86a791ed0dfed5a41aecf3c83d956e51
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-04-20 22:37:17 +0000

    added task 17

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""471#17- Integrate"""
+
+
+def poly_integral(poly, C=0):
+    """calculates the integral of a polynomial"""
+    if (not isinstance(poly, list) or
+       (not isinstance(C, (int, float))) or any
+       (not isinstance(x, (int, float)) for x in poly)):
+            return None
+    integral = [C]
+    if poly != [0]:
+        integral = [float(C)] + [poly[i] / (i + 1) for i in range(len(poly))]
+    return [float(x) if not x.is_integer() else int(x) for x in integral]
+
