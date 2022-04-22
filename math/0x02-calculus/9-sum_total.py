c317b4323bded72ac22973a50e9ecc941150d42b
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-04-22 17:31:43 +0000

    updated task 9 - trying to get expected result

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """calculates sum_{i=1}^{n} i^2"""
-    if not isinstance(n, int):
+    if not isinstance(n, int) or n < 1:
         return None
     return sum(map(lambda i: i ** 2, range(1, n + 1)))
