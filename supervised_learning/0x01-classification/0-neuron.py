2837e65b3554642aeeef64f618dd5a41359042e1
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-05-09 22:45:06 +0000

    updated task 0

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -10,9 +10,9 @@ class Neuron:
     def __init__(self, nx):
         """class constructor"""
         if not isinstance(nx, int):
-            raise TypeError("nx must be a integer")
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError("nx must be positive")
-        self.W = np.random.normal(0, 1, (1, nx))
+            raise ValueError("nnx must be a positive integer")
+        self.W = np.random.normal(size=(1, nx))
         self.b = 0
         self.A = 0
