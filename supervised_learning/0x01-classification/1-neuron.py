12e1fdb35b825d19813931135da7b60d2519596c
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-05-09 22:50:59 +0000

    added task 1

---
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""475#1 - Privatize Neuron"""
+
+import numpy as np
+
+
+class Neuron:
+    """defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """class constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """resturns the weights vector for the neuron"""
+        return self.__W
+
+    @property
+    def b(self):
+        """returns the bias for the neuron"""
+        return self.__b
+
+    @property
+    def A(self):
+        """returns the activated output of the neuron"""
+        return self.__A
