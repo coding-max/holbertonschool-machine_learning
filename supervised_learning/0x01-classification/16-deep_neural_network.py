a149814a491c0da7e18ddd770e112f75237946c4
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-05-10 21:14:44 +0000

    updated task 16 to use only one loop

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -14,13 +14,13 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if (not isinstance(layers, list) or len(layers) == 0 or
-           any(not isinstance(layer, int) for layer in layers) or
-           any(layer < 1 for layer in layers)):
+        if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError('layers must be a list of positive integers')
         self.L = len(layers)
         self.cache = self.weights = {}
         for i in range(self.L):
+            if not isinstance(layers[i], int) or layers[i] < 1:
+                raise TypeError('layers must be a list of positive integers')
             w = 'W' + str(i + 1)
             b = 'b' + str(i + 1)
             prev = layers[i - 1] if i else nx
