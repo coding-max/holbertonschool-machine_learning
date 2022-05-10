0a35094c7b7650b18dd1565501dc82b5737653d3
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-05-10 22:09:10 +0000

    added task 17

---
+++ supervised_learning/0x01-classification/17-deep_neural_network.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""475#17 - Privatize DeepNeuralNetwork"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ defines a deep neural network
+    performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """class constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for i in range(self.L):
+            if not isinstance(layers[i], int) or layers[i] < 1:
+                raise TypeError('layers must be a list of positive integers')
+            w = 'W' + str(i + 1)
+            b = 'b' + str(i + 1)
+            prev = layers[i - 1] if i else nx
+            sqrt = np.sqrt(2 / prev)
+            self.__weights[w] = np.random.randn(layers[i], prev) * sqrt
+            self.__weights[b] = np.zeros(shape=(layers[i], 1))
+
+    @property
+    def L(self):
+        """returns the number of layers in the neural network"""
+        return self.__L
+
+    @property
+    def weights(self):
+        """returns a dict with all intermediary values of the network"""
+        return self.__weights
+
+    @property
+    def cache(self):
+        """returns a dict with all weights and biased of the network"""
+        return self.__cache
