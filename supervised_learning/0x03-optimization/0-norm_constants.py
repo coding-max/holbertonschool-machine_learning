4ab17a894dc460de5dee16f42918bfe85372992f
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-05-19 00:40:33 +0000

    added task 0

--- supervised_learning/0x03-optimization/0-norm_constants.py
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -2,12 +2,8 @@
 """487#0"""
 
 import numpy as np
-import statistics
 
 
 def normalization_constants(X):
     """calculates the normalization (standardization) constants of a matrix"""
-    mean = np.mean(X, axis=0)
-    variance = np.mean((X-mean) ** 2, axis=0)
-
-    return mean, variance
+    return np.mean(X, axis=0), np.std(X, axis=0)
