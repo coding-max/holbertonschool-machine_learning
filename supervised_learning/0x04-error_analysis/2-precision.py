26838566b91598e72a2c584f7d6d625e631f3e43
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-05-23 21:56:34 +0000

    added task 2

---
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""489#2"""
+
+import numpy as np
+
+
+def precision(confusion):
+    """calculates the precision for each class in a confusion matrix"""
+    return np.diagonal(confusion) / np.sum(confusion, axis=0)
