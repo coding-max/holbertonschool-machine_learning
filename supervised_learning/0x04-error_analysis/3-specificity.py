b8cc61f599dbe1c7347f76bb81e406912d1306b3
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-05-23 22:16:09 +0000

    added task 3

---
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""489#3"""
+
+import numpy as np
+
+
+def specificity(confusion):
+    """calculates the specificity (TNR) for each class in a confusion matrix"""
+    FP = confusion.sum(axis=0) - np.diag(confusion)
+    FN = confusion.sum(axis=1) - np.diag(confusion)
+    TP = np.diag(confusion)
+    TN = confusion.sum() - (FP + FN + TP)
+    return TN / TN + FP
